e2f5d780e56bb087fef921a3f50f974ca8412cc2
Georgian <georgian2all@yahoo.com>
2016-07-14 18:59:48 +0300

    Add files via upload

---
+++ lcomprehensions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Python 2.7
+Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]. Write one line of Python that takes this list a and makes a new list that has only the even elements of this list in it.
+"""
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+b = [item for item in a if (item%2 != 0)]
+print b
